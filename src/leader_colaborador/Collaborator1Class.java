6ef5e43f73c49aea91add69fd46a1e3d0d0b65ab
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2018-11-13 09:00:40 +0100

    C1 he creado una clase con comentarios

---
+++ src/leader_colaborador/Collaborator1Class.java
@@ -0,0 +1,13 @@
+package leader_colaborador;
+
+/**
+ *
+ * @author jmartinezpineiro
+ */
+public class Collaborator1Class {
+    
+    /*Hola esto es una prueba de que las Branch mola mucho, esta clase
+    mola un monton 
+    */
+
+}
