4dc5cd9e7441f319331ffa4e4a748d875cc707ed
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-11-13 08:48:08 +0100

    B.COMMIT

--- src/leader_colaborador/Leader_Colaborador.java
+++ src/leader_colaborador/Leader_Colaborador.java
@@ -14,7 +14,7 @@ public class Leader_Colaborador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+//ESTA EN ESTE REPOSITORIO DEL GITHUB https://github.com/laravdorna/RamaEnColaboracion.git
     }
 
 }
