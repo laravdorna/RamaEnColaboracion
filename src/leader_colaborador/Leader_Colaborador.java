c831483f2f87f642a0b562eb11b73abcc1b0fc7a
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-11-13 08:47:00 +0100

    A.COMMIT

---
+++ src/leader_colaborador/Leader_Colaborador.java
@@ -0,0 +1,20 @@
+/*
+ *EJERCICIO CONTORNO CREAR RAMAS CON COLABORACION 
+ */
+
+package leader_colaborador;
+
+/**
+ *
+ * @author Lara Vazquez Dorna
+ */
+public class Leader_Colaborador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
